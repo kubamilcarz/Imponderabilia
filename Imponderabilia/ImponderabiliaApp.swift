eec0171d7af83fa9ebb29f25d27f6af32c6f2b25
Kuba Milcarz <kubamilcarz@yahoo.com>
2023-09-14 10:56:07 -0400

    Initial Commit

---
+++ Imponderabilia/ImponderabiliaApp.swift
@@ -0,0 +1,20 @@
+//
+//  ImponderabiliaApp.swift
+//  Imponderabilia
+//
+//  Created by Kuba Milcarz on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ImponderabiliaApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
